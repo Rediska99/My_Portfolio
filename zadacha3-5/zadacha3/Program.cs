aeda49b74b2e00044e2d7863d5fb75779ba2890e
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-11-17 01:13:56 +0300

    Add files via upload

---
+++ zadacha3-5/zadacha3/Program.cs
@@ -0,0 +1,22 @@
+﻿//С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+using System;
+
+namespace zadacha3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a,b;
+            Console.WriteLine("Введите первое число:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            b = int.Parse(Console.ReadLine());
+            if(a > b)
+                Console.WriteLine("Число a больше.");
+            else
+                Console.WriteLine("Число b больше.");
+        }
+    }
+}

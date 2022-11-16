7e7f53e58e89f3bc06f86e04c63b77cd1cc1f4d9
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-11-17 01:19:05 +0300

    Add files via upload

---
+++ 12-15/zadacha15/Program.cs
@@ -0,0 +1,28 @@
+﻿//15.С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+using System;
+
+namespace zadacha15
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a >= 100 & a <= 999)
+                Console.WriteLine("Остаток:" + a % 10);
+            else
+            {
+                if(a >= 1000)
+                {
+                    a = a / 10;
+                    {
+                        Console.WriteLine("статок:" + a % 10);
+                    }
+                }
+                  
+            }
+            if(a >= 0 & a <= 99)
+                Console.WriteLine("Числа нет");
+        }
+    }
+}

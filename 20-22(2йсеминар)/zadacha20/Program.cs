e181a3a98ca89fea66466e2fda22aee2df5c0732
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-11-24 00:38:59 +0300

    Add files via upload

---
+++ 20-22(2йсеминар)/zadacha20/Program.cs
@@ -0,0 +1,34 @@
+﻿//20)Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+using System;
+
+namespace zadacha20
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            if (X < 0 & Y > 0 )
+            {
+                Console.WriteLine("Первая четверть");
+            }
+            else
+                if(X > 0 & Y > 0)
+                {
+                    Console.WriteLine("Вторая четверть");
+                }
+            else
+                if(X < 0 & Y < 0)
+                {
+                    Console.WriteLine("Третья четверть");
+                }    
+            else
+                if(X > 0 & Y < 0)
+                {
+                    Console.WriteLine("Четвертая четверть");
+                }    
+        }
+    }
+}
---
+++ 20-22(2йсеминар)/zadacha22/Program.cs
@@ -0,0 +1,40 @@
+﻿//22)Программа проверяет пятизначное число на палиндромом.
+
+using System;
+
+namespace Project;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            {
+                int number = 52651;
+                if(number > 9999 && number < 100000)
+                {
+                    int digitOne = number % 10;
+                    int digitTen = number / 10 % 10;
+                    int digitThousend = number / 1000 % 10;
+                    int digitTenThousend = number / 10000;
+                    if(digitOne == digitTenThousend && digitTen == digitThousend)
+                    {
+                        Console.WriteLine("Число полиндром");
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Число не полиндром");
+                    }
+
+                }   
+                else
+                {
+                    Console.WriteLine("Введено не пятизначное число");
+                } 
+            }
+                
+              
+
+        }    
+    }    

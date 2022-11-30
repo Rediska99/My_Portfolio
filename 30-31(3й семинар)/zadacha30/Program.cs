07521caf22eaa86d18ed9a4e54dc03feb1be3676
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-12-01 02:22:13 +0300

    Add files via upload

---
+++ 30-31(3й семинар)/zadacha30/Program.cs
@@ -0,0 +1,18 @@
+﻿//30)Написать программу вычисления произведения чисел от 1 до N
+
+using System;
+
+namespace zadacha24
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+           int N = int.Parse(Console.ReadLine());
+           int k = 1;
+           for(int i = 1; i <= N; i++)
+               k *= i;
+           Console.WriteLine("Произведение чисел:" + k);
+        }  
+    }
+}
---
+++ 30-31(3й семинар)/zadacha31/Program.cs
@@ -0,0 +1,18 @@
+﻿//31)Показать кубы чисел, заканчивающихся на четную цифру
+
+using System;
+
+namespace zadacha31
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a % 2 == 0)
+                Console.WriteLine($"{a} ^ {3} = {Math.Pow(a , 3)}");
+            else
+                Console.WriteLine("Ошибка");    
+        }
+    }
+}

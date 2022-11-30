acb2a9ebd013659bd8691800586e1b0ae9deb63f
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-12-01 02:11:40 +0300

    Add files via upload

---
+++ 27-29(3й семинар)/zadacha28/Program.cs
@@ -0,0 +1,21 @@
+﻿//28)Определить количество цифр в числе. Сделать подпрограмму.
+
+using System;
+
+namespace zadacha27
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+          int a = int.Parse(Console.ReadLine());
+          int i = 0;
+          if(a >= 0 || a <= 0)
+          {
+            i++;
+            a /= 10;
+          }
+          Console.WriteLine("Количество цифр в числе: " + i);
+        }
+    }
+}
---
+++ 27-29(3й семинар)/zadacha29/Program.cs
@@ -0,0 +1,21 @@
+﻿//29)Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+using System;
+
+namespace zadacha27
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+          int a = int.Parse(Console.ReadLine());
+          int sum = 0;
+          while(a > 0)
+          {
+            sum += a % 10;
+            a /= 10;
+          }
+          Console.WriteLine("Сумма цифр в числе: " + sum);
+        }
+    }
+}

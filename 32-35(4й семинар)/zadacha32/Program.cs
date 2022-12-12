6344dec73d60e48e28808d11e2c3daeab47c9cb2
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-12-13 02:27:37 +0300

    Add files via upload

---
+++ 32-35(4й семинар)/zadacha32/Program.cs
@@ -0,0 +1,16 @@
+﻿//32)Задать массив из 8 целых элементов и вывести их на экран
+
+using System;
+
+namespace zadacha32
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int []a = {1,2,3,4,5,6,7,8};
+            for(int i = 0; i <= a.Length; i++)
+            Console.WriteLine(i);
+        }
+    }
+}

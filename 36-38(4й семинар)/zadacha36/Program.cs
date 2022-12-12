4f2cfa7072a4b8e5687f897cf38dc40403db7047
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-12-13 02:31:29 +0300

    Add files via upload

---
+++ 36-38(4й семинар)/zadacha36/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadacha36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string[] array = { "Коля", "Федя", "Фрося", "Мотя" };
+            name=Console.ReadLine();
+            foreach (string str in array)
+            {
+                if (str == name)
+                    Console.WriteLine(string.Format("Слово '{0}' содержится в массиве", name));
+            }
+        }
+    }
+}        
---
+++ 36-38(4й семинар)/zadacha38/Program.cs
@@ -0,0 +1,26 @@
+﻿//38)В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+using System;
+
+namespace zadacha38
+{
+    class Program
+    {
+        static void Main(string []args)
+        {
+            int N=5;
+            int [] a=new int[N];
+            Random random=new Random();
+            for(int i=0;i<a.Length;i++)
+                a[i]=random.Next(0,300);
+            for(int i=0;i<a.Length;i++)
+                Console.WriteLine($"{a[i],5}");
+            int count=0;
+            for(int i=0;i<a.Length;i++)
+                if(a[i]>=10 && a[i]<=99)
+                    count++;
+            Console.WriteLine();
+            Console.WriteLine(count);                 
+        }
+    }
+}

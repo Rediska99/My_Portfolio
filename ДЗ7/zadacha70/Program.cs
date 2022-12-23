62816c9f704aa80c3dc6683468f6056661c3b19a
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-12-23 19:56:25 +0300

    Add files via upload

---
+++ ДЗ7/zadacha70/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace zadacha70
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N=int.Parse(Console.ReadLine());
+            for(int i=1;i<=N;i++)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ ДЗ7/zadacha71/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace zadacha70
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N=int.Parse(Console.ReadLine());
+            for(int i=N;i>=1;i=i-1)
+                Console.WriteLine(i);
+        }
+    }
+}
